23dd1b78a888dd03f8914c71a88cfbb2490304fd
Nay0ne <nayone606@googlemail.com>
2023-06-16 11:06:19 +0100

    added first test-returns a cashcard

--- src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
+++ src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
@@ -2,12 +2,23 @@ package nullpointexceptionals.CashCard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CashcardApplicationTests {
 
+	@Autowired
+    TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+    void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
