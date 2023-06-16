d3a3346eccf38b642289bc05cdb367d765d1543d
Dominic Douglas <102813489+domdouglas@users.noreply.github.com>
2023-06-16 15:49:48 +0100

    Update CashCardController.java

--- src/main/java/nullpointexceptionals/CashCard/CashCardController.java
+++ src/main/java/nullpointexceptionals/CashCard/CashCardController.java
@@ -1,4 +1,4 @@
-package nullpointexceptionals.cashcard;
+package nullpointexceptionals.CashCard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
