039db3e2db08359c1343337021ee54d4eae80683
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-16 14:32:11 +0100

    created seed data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO cash_card (amount, owner) VALUES (100.0, 'Natalie');
+INSERT INTO cash_card (amount, owner) VALUES (200.0, 'Domiique');
+INSERT INTO cash_card (amount, owner) VALUES (300.0, 'Jeff');
+INSERT INTO cash_card (amount, owner) VALUES (400.0, 'John');
+INSERT INTO cash_card (amount, owner) VALUES (500.0, 'Tim');
+INSERT INTO cash_card (amount, owner) VALUES (600.0, 'Mohamed');
