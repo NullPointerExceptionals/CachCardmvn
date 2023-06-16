0ce44ed4d29928729f6a52bde2165723b6bf7efc
Nay0ne <nayone606@googlemail.com>
2023-06-16 12:55:20 +0100

    add to return a cashcard test

--- src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
+++ src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
@@ -1,15 +1,20 @@
 package nullpointexceptionals.CashCard;
 
+import com.jayway.jsonpath.JsonPath;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import com.jayway.jsonpath.DocumentContext;
+
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
 
 	@Autowired
@@ -19,6 +24,14 @@ class CashcardApplicationTests {
     void shouldReturnACashCardWhenDataIsSaved() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(99);
+
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(123.45);
+
 	}
 
 }
