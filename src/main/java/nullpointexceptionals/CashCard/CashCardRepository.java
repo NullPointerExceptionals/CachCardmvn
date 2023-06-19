abb0daafc6da6b57f778f925604e124044dc11da
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-19 11:15:59 +0100

    Created findByOwner method

--- src/main/java/nullpointexceptionals/CashCard/CashCardRepository.java
+++ src/main/java/nullpointexceptionals/CashCard/CashCardRepository.java
@@ -9,4 +9,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface CashCardRepository extends CrudRepository<CashCard, Long> {
     List<CashCard> findByOwner(String owner);
 
+    CashCard findByOwnerAndId(String owner, Long id);
+
 }
