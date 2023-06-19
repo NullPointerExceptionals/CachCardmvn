d5418bfbe753ce597fcc4ccc0b4ff4cabbc3e87e
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-20 00:10:35 +0100

    created findByName method

--- src/main/java/nullpointexceptionals/CashCard/AuthUserRepository.java
+++ src/main/java/nullpointexceptionals/CashCard/AuthUserRepository.java
@@ -3,5 +3,5 @@ package nullpointexceptionals.CashCard;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AuthUserRepository extends CrudRepository<AuthUser, Long> {
-
+    AuthUser findByName(String name);
 }
