e2f2cbffbd9efec382e6bb8b52f13c0ac4d319cc
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-19 11:02:47 +0100

    GET mapping for find by owner

--- src/main/java/nullpointexceptionals/CashCard/CashCardController.java
+++ src/main/java/nullpointexceptionals/CashCard/CashCardController.java
@@ -1,12 +1,25 @@
 package nullpointexceptionals.CashCard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 import java.net.URI;
 
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.UriComponentsBuilder;
 
 
+
+import main.java.nullpointexceptionals.CashCard;
+
+import java.util.List;
+import java.util.Optional;
 
 
 
@@ -18,19 +31,7 @@ public class CashCardController {
    public CashCardController(CashCardRepository cashCardRepository) {
       this.cashCardRepository = cashCardRepository;
    }
+
    private CashCard findCashCard(Long requestedId) {
-        return cashCardRepository.findById(requestedId).orElse(null);
-}
-
-@GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        CashCard cashCard = findCashCard(requestedId);
-        if (cashCard != null) {
-            return ResponseEntity.ok(cashCard);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-   
-      }
-
+    return cashCardRepository.findByIdAndOwner(requestedId);
 }
