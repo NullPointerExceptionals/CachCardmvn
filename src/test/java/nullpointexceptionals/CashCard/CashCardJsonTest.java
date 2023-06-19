73e14e857f8a7f0dfa31e0d612792ed8ad29ef30
Nay0ne <nayone606@googlemail.com>
2023-06-19 12:33:10 +0100

    failing test- no json set yet

--- src/test/java/nullpointexceptionals/CashCard/CashCardJsonTest.java
+++ src/test/java/nullpointexceptionals/CashCard/CashCardJsonTest.java
@@ -1,5 +1,29 @@
 package nullpointexceptionals.CashCard;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
 public class CashCardJsonTest {
-    
+
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+    @Test
+    public void cashCardSerializationTest() throws IOException {
+        CashCard cashCard = new CashCard(1L, 100.0, "Natalie");
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(1);
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
+             .isEqualTo(100.00);
+    }
 }
