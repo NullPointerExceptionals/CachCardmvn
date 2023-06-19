f5776d6091ab2f21fef43c7e836e05c4aff9b48e
Nay0ne <nayone606@googlemail.com>
2023-06-19 14:54:19 +0100

    return A cashcard test

--- src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
+++ src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
@@ -1,13 +1,41 @@
 package nullpointexceptionals.CashCard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 
-@SpringBootTest
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
-	}
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isEqualTo(1);
+
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(100.0);
+
+        String owner = documentContext.read("$.owner");
+        assertThat(owner).isEqualTo("Natalie");
+    }
 
 }
