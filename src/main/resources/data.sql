6284a221b27d0b699481aa8a67c8e9265cf14ab5
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-19 15:14:45 +0100

    Intial seed data for transaction table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,10 @@ INSERT INTO cash_card (amount, owner) VALUES (600.0, 'Jeff');
 INSERT INTO cash_card (amount, owner) VALUES (800.0, 'John');
 INSERT INTO cash_card (amount, owner) VALUES (1000.0, 'Tim');
 INSERT INTO cash_card (amount, owner) VALUES (1200.0, 'Mohamed');
+
+INSERT INTO transaction (cash_card_id, amount_added, amount_removed) VALUES (1, 50.0, 0.0);
+INSERT INTO transaction (cash_card_id, amount_added, amount_removed) VALUES (2, 0.0, 25.0);
+INSERT INTO transaction (cash_card_id, amount_added, amount_removed) VALUES (3, 75.0, 0.0);
+INSERT INTO transaction (cash_card_id, amount_added, amount_removed) VALUES (1, 500.0, 0.0);
+INSERT INTO transaction (cash_card_id, amount_added, amount_removed) VALUES (2, 0.0, 250.0);
+INSERT INTO transaction (cash_card_id, amount_added, amount_removed) VALUES (3, 750.0, 0.0);
