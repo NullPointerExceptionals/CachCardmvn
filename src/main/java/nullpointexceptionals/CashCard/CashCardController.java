995c993957e5e329df72338366e8e89d81763f3e
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-19 11:02:47 +0100

    GET mapping for find by owner

--- src/main/java/nullpointexceptionals/CashCard/CashCardController.java
+++ src/main/java/nullpointexceptionals/CashCard/CashCardController.java
@@ -1,7 +1,9 @@
 package nullpointexceptionals.CashCard;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.net.URI;
 import java.security.Principal;
+import java.security.Principal;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.UriComponentsBuilder;
 
+// import main.java.nullpointexceptionals.CashCard;
 // import main.java.nullpointexceptionals.CashCard;
 
 import java.util.List;
@@ -27,6 +31,7 @@ public class CashCardController {
    public CashCardController(CashCardRepository cashCardRepository) {
       this.cashCardRepository = cashCardRepository;
    }
+
 
    private CashCard findCashCard(Long requestedId) {
       return cashCardRepository.findById(requestedId).orElse(null);
@@ -51,16 +56,6 @@ public class CashCardController {
          return ResponseEntity.notFound().build();
       }
       return ResponseEntity.ok(cashCards);
-   }
-
-   @GetMapping("/owner/{owner}/{id}")
-   public ResponseEntity<CashCard> findByOwnerAndId(@PathVariable String owner, @PathVariable Long id) {
-      CashCard cashCard = cashCardRepository.findByOwnerAndId(owner, id);
-      if (cashCard != null) {
-         return ResponseEntity.ok(cashCard);
-      } else {
-         return ResponseEntity.notFound().build();
-      }
    }
 
 }
