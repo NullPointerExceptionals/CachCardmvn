05547084901d007df664519580a26e77526992b7
Nay0ne <nayone606@googlemail.com>
2023-06-19 12:02:12 +0100

    appl tests updated

--- src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
+++ src/test/java/nullpointexceptionals/CashCard/CashcardApplicationTests.java
@@ -2,10 +2,14 @@ package nullpointexceptionals.CashCard;
 
 import com.jayway.jsonpath.JsonPath;
 
+import net.minidev.json.JSONArray;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import com.jayway.jsonpath.DocumentContext;
 
@@ -13,6 +17,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
@@ -41,5 +47,19 @@ class CashcardApplicationTests {
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
     }
+
+    @Test
+	@DirtiesContext
+	void shouldDeleteAnExistingCashCard() {
+    	ResponseEntity<Void> response = restTemplate
+            .exchange("/cashcards/1", HttpMethod.DELETE, null, Void.class);
+    	assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+		ResponseEntity<String> getResponse = restTemplate
+            .getForEntity("/cashcards/1", String.class);
+    	assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	
+	}
+
+    
 
 }
