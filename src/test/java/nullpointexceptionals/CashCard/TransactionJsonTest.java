6b4da2236222b3e61daafb8d51b8c82c7ed0082b
Nay0ne <nayone606@googlemail.com>
2023-06-20 09:09:07 +0100

    trans json tests start

--- src/test/java/nullpointexceptionals/CashCard/TransactionJsonTest.java
+++ src/test/java/nullpointexceptionals/CashCard/TransactionJsonTest.java
@@ -25,8 +25,49 @@ public class TransactionJsonTest {
     @BeforeEach
     void setUp() {
         transactions = Arrays.array(
-            new Transaction(1L, 50.0, 0.0),
+            new Transaction(1L, 1L, 50.0, 0.0),
+            new Transaction(4L, 1L, 500.0, 0.0),
+            new Transaction(2L, 2L, 0.0, 25.0),
+            new Transaction(5L, 2L, 0.0, 250.0),
+            new Transaction(3L, 3L, 75.0, 0.0),
+            new Transaction(6L, 3L, 750.0, 0.0)
         );
+    }
+
+    @Test
+    public void transactionSerializationTest() throws IOException {
+        Transaction transaction = transactions[0];
+        assertThat(json.write(transaction)).isStrictlyEqualToJson("transaction.json");
+        assertThat(json.write(transaction)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(transaction)).extractingJsonPathNumberValue("@.id")
+        .isEqualTo(1);
+        assertThat(json.write(transaction)).hasJsonPathNumberValue("@.cashCardId");
+        assertThat(json.write(transaction)).extractingJsonPathNumberValue("@.cashCardId")
+        .isEqualTo(1);
+        assertThat(json.write(transaction)).hasJsonPathNumberValue("@.amountAdded");
+        assertThat(json.write(transaction)).extractingJsonPathNumberValue("@.amountAdded")
+        .isEqualTo(50.0);
+        assertThat(json.write(transaction)).hasJsonPathNumberValue("@.amountRemoved");
+        assertThat(json.write(transaction)).extractingJsonPathNumberValue("@.amountRemoved")
+        .isEqualTo(0.0);
+    }
+
+    @Test
+    public void transactionDeserializationTest() throws IOException {
+        String expected = """
+            {
+                "id": 1,
+                "cashCardId": 1,
+                "amountAdded": 50.0,
+                "amountRemoved": 0.0
+            }
+            """;
+        assertThat(json.parse(expected)).isEqualTo(new Transaction(1L, 1L, 50.0, 0.0));
+        assertThat(json.parseObject(expected).id()).isEqualTo(1);
+        assertThat(json.parseObject(expected).cashCardId()).isEqualTo(1);
+        assertThat(json.parseObject(expected).amountAdded()).isEqualTo(50.0);
+        assertThat(json.parseObject(expected).amountRemoved()).isEqualTo(0.0);
+
     }
     
 }
