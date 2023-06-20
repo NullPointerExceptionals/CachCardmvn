91237777a88aebcda114b1cd35ac2cf8dd9cb33c
JLKennard <128471690+JLKennard@users.noreply.github.com>
2023-06-20 11:59:14 +0100

    Users card title updated

--- src/main/resources/static/src/components/CardsView.js
+++ src/main/resources/static/src/components/CardsView.js
@@ -4,6 +4,7 @@ import { CardsList } from "./CardsList";
 export const CardsView = ({ cardsData }) => {
   return (
     <div>
+      <h1>{cardsData[0].owner}'s Cash Cards</h1>
       <CardsList cardsData={cardsData} />
     </div>
   );
