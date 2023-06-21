014da2e85aa5551e4bbbe58c8979ca3d06e7302c
Nay0ne <nayone606@googlemail.com>
2023-06-21 12:13:45 +0100

    auth json array input to java file

--- src/test/java/nullpointexceptionals/CashCard/AuthUserJsonTest.java
+++ src/test/java/nullpointexceptionals/CashCard/AuthUserJsonTest.java
@@ -1,5 +1,42 @@
 package nullpointexceptionals.CashCard;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
 public class AuthUserJsonTest {
     
+    @Autowired
+    private JacksonTester<AuthUser> json;
+
+    @Autowired private JacksonTester<AuthUser[]> jsonList;
+
+    private AuthUser[] authUsers;
+
+    @BeforeEach
+    void setUp() {
+        authUsers = Arrays.array(
+            new AuthUser(1L, "Alice", 1L),
+            new AuthUser(2L, "Bob", 2L),
+            new AuthUser(3L, "Charlie", 3L),
+            new AuthUser(4L, "David", 4L),
+            new AuthUser(5L, "Eve", 5L),
+            new AuthUser(6L, "Frank", 6L),
+            new AuthUser(7L, "Grace", 7L),
+            new AuthUser(8L, "Henry", 8L),
+            new AuthUser(9L, "Isabella", 9L),
+            new AuthUser(10L, "Jack", 10L),
+            new AuthUser(11L, "Kate", 10L),
+            new AuthUser(12L, "Liam", 12L)
+        );
+    }
 }
+
